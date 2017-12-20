4a6e405d28dd42349011a52b611e116937bf629b
Robert <robop806@student.liu.se>
2017-12-20 15:25:30 +0100

    uppdaterad mc path som är nästan klar, bara lite vektorgrejs kvar

--- main/monte_carlo_path.py
+++ main/monte_carlo_path.py
@@ -2,14 +2,22 @@
 
 import math
 import normal_approximation
-import numpy.linalg
+import numpy as np
 
 def MonteCarloPath(S0, corr, rate, vol, tVec):
     """Antag att alla invariabler är arrayer"""
     St = S0
-    L = numpy.linalg.cholesky(corr) # skapar en 3x3 cholesky av corrmatris
-#    u = random skapa här 3xlen(tVec) normal random
-    u_corr = numpy.dot(L,u) # gör matrismultiplication
-    """Det som är kvar: loopa och skapa path, förutsatt att allt funkar"""
+    path = St
+    n = len(tVec) # Längen, möjligen fel om 1xN ist för Nx1
 
+    """Gör korrelereade slumptal"""
+    L = np.linalg.cholesky(corr) # skapar en 3x3 lower triangular av corrmatris
+    u = np.random.normal(0.0, 1.0, (len(corr), n)) # Här ska vi ha en 3xN
+    u_corr = np.dot(L,u) # gör slumptalen korrelerade enligt matris
 
+    """Skapa the path"""
+    for i in range(n - 1)
+        dt = tVec[ i + 1 ] - tVec[ i ]
+        # 2do: skapa värde på St, gärna i vektorform dvs en kolonn åt gången så man slipper inre loop
+        # append till path
+    return path
